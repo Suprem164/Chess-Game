479a276647ab3747736347794dadbdc669c922af
Lawarna Aree <supremaree@gmail.com>
2025-12-23 18:48:15 +0545

    Made the chess board with 8x8 layout and proper indexing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Chessboard from '../src/components/Chessboard.jsx'
+import '../src/App.css'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className="app">
+      <Chessboard />
+    </div>
   )
 }
 
---
+++ src/components/Chessboard.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import '../styles/Chessboard.css'
+
+const Chessboard = () => {
+
+    const xAxis = ["a", "b", "c", "d", "e", "f", "g", "h"]
+    const yAxis = ["1", "2", "3", "4", "5", "6", "7", "8"]
+    let board = []
+
+    for (let j = yAxis.length - 1; j >= 0; j--) {
+        for (let i = 0; i < xAxis.length; i++) {
+            if ((i + j + 2) % 2 == 0) {
+                board.push(
+                    <div className="tile black-tile">[{xAxis[i]}{yAxis[j]}]</div>
+                )
+            }
+
+            else {
+                board.push(
+                    <div className="tile white-tile">[{xAxis[i]}{yAxis[j]}]</div>
+                )
+            }
+        }
+    }
+
+    return (
+        <div className="chess-board">{board}</div>
+    )
+}
+
+export default Chessboard
